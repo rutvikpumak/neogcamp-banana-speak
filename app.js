908820dacc29e76495daa01fad2e9374b1263b31
Rutvik Umak <rutvikumak@gmail.com>
2021-11-23 23:05:53 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var  txtInput= document.querySelector("#textInput")
+var  txtOutput= document.querySelector("#output")
+
+btnTranslate.addEventListener("click",()=>{
+    console.log(txtInput.value)
+})
