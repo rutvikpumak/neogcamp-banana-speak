224cb60e2ef9250d6f155af72056461db5319197
Rutvik Umak <rutvikumak@gmail.com>
2021-11-24 09:06:35 +0530

    feat: Added API calls

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ var btnTranslate = document.querySelector("#btn-translate")
 var  txtInput= document.querySelector("#textInput")
 var  txtOutput= document.querySelector("#output")
 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslateURL(text){
+    return `${serverUrl}?text=${text}`;
+}
+
+function errorHandler(){
+    alert("Something wrong with serve ! Please try again later.");
+}
 btnTranslate.addEventListener("click",()=>{
-    console.log(txtInput.value)
+    var inputText = txtInput.value;
+
+    fetch(getTranslateURL(inputText))
+    .then(response=>response.json())
+    .then(json=>{
+        txtOutput.innerText =json.contents.translated})
+    .catch(errorHandler)
 })
